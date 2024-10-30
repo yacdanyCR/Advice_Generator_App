8c32f5fcd65930e345ac5c858d08f85e4828c63a
Yacdany <jack20-21@hotmail.com>
2024-10-30 13:16:28 -0600

    Bug loader

--- app/Component/LoaderComponen.js
+++
@@ -1,5 +0,0 @@
-const LoaderComponent = () => {
-  return `<div class="lds-ring">Loading...<div></div><div></div><div></div><div></div></div>`;
-};
-
-export { LoaderComponent };
--- app/app.js
+++ app/app.js
@@ -1,27 +1,25 @@
-import { LoaderComponent } from "./Component/LoaderComponen.js";
 import { handleAdvice } from "./Services/handleAdvice.js";
 
 const $advice__Title = document.querySelector(".advice__Title");
 const $advice__Mesage = document.querySelector(".advice__Mesage");
 const $btn_Advice = document.querySelector("#btn_Advice");
+const $loader = document.querySelector(".lds-ring");
 const loaderState = {
   on: "block",
   off: "none",
 };
 
 window.addEventListener("DOMContentLoaded", async () => {
-  $advice__Mesage.innerHTML = LoaderComponent();
-  const $loader = document.querySelector(".lds-ring");
   await handleAdvice($advice__Title, $advice__Mesage);
   $loader.style.display = loaderState.off;
 
   $btn_Advice.addEventListener("click", async () => {
+    $loader.style.display = loaderState.on;
+
     while ($advice__Title.firstChild && $advice__Mesage.firstChild) {
       $advice__Title.removeChild($advice__Title.firstChild);
       $advice__Mesage.removeChild($advice__Mesage.firstChild);
     }
-    $advice__Mesage.innerHTML = LoaderComponent();
-    const $loader = document.querySelector(".lds-ring");
     await handleAdvice($advice__Title, $advice__Mesage);
     $loader.style.display = loaderState.off;
   });
